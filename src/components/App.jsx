2bc77ff740224f22e839f909e5cd24b7a0f62bce
dozeran <dozeran@proton.me>
2023-10-28 22:53:37 +0300

    second homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,84 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import axios from 'axios';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Loader from './Loader/Loader';
+import Button from './Button/Button';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '39382109-d071ce59cf94359f9a44c3b97';
+
+class App extends Component {
+  state = {
+    photos: [],
+    query: '',
+    page: 1,
+    isLoading: false,
+    isButton: false,
+  };
+
+  fetchPhotos = async (newQuery, currentPage) => {
+    this.setState({ isLoading: true });
+    try {
+      const response = await axios.get(BASE_URL, {
+        params: {
+          key: API_KEY,
+          q: newQuery,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          page: currentPage,
+          per_page: 12,
+        },
+      });
+      if (response.data.total === 0) {
+        this.setState({
+          photos: [],
+          isButton: false,
+        });
+        return;
+      }
+
+      this.setState(prevState => ({
+        photos: [...prevState.photos, ...response.data.hits],
+        query: newQuery,
+        page: currentPage + 1,
+        isButton: true,
+      }));
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  updateQuery = query => {
+    this.setState({ isLoading: true });
+    this.fetchPhotos(query, 1);
+  };
+
+  loadMore = () => {
+    this.setState({ isLoading: true });
+    this.fetchPhotos(this.state.query, this.state.page);
+  };
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.query !== prevState.query) {
+      this.setState({ page: 1, photos: [] });
+      this.fetchPhotos(this.state.query, 1);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Searchbar onQueryChange={this.updateQuery} />
+        <ImageGallery photos={this.state.photos} />
+        <Loader isLoading={this.state.isLoading} />
+        <Button isButton={this.state.isButton} loadMore={this.loadMore} />
+      </>
+    );
+  }
+}
+
+export default App;
